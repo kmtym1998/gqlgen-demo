f1aaa4031987059a3236883a502a90b5ee0db607
kmtym1998 <kmtym.devloper@gmail.com>
2021-07-09 13:35:40 +0900

    dockerに載せたのでDBサーバのホストの指定方法を変更

--- graph/pkg/postgres/postgres.go
+++ graph/pkg/postgres/postgres.go
@@ -12,7 +12,7 @@ var (
 
 func Open() (*gorm.DB) {
 	// TODO: 環境変数を使う
-	dsn := "host=localhost user=hasura password=secret dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Tokyo"
+	dsn := "host=host.docker.internal user=hasura password=secret dbname=postgres port=5432 sslmode=disable TimeZone=Asia/Tokyo"
 	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		print("DB接続失敗", err)
